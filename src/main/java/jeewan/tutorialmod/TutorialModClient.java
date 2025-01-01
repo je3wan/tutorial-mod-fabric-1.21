c8157899089a1449e073fa44f7acd4c78099ed82
je3wan <jeewanr@proton.me>
2025-01-01 18:16:37 +0100

    2nd try

---
+++ src/main/java/jeewan/tutorialmod/TutorialModClient.java
@@ -0,0 +1,9 @@
+package jeewan.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
